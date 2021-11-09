abdf709e17a228d4f2df659a8720443ee55cdfbb
Aditya Trivedi <adityakuldeeptrivedi@gmail.com>
2021-11-09 20:11:59 +0530

    Sign Up Page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,84 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>
-      <h1>Signup Page</h1>
-    </div>
+    <section className="signup">
+      <div className="container mt-5">
+        <div className="signup-content">
+          <div className="signup-form">
+            <h2 className="form-title">Sign Up</h2>
+            <form className="registeration-form">
+              <div className="form-group">
+                <label htmlFor="name">
+                  <i class="zmdi zmdi-account material-icons-name"></i>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  autoComplete="off"
+                  placeholder="Your Name"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="email">
+                  <i class="zmdi zmdi-email material-icons-name"></i>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="off"
+                  placeholder="Your Email"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password">
+                  <i class="zmdi zmdi-lock material-icons-name"></i>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  autoComplete="off"
+                  placeholder="Your Password"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="cpassword">
+                  <i class="zmdi zmdi-lock material-icons-name"></i>
+                </label>
+                <input
+                  type="password"
+                  name="cpassword"
+                  id="cpassword"
+                  autoComplete="off"
+                  placeholder="Confirm Password"
+                />
+              </div>
+
+              <div className="form-group form-button">
+                <input
+                  type="submit"
+                  name="signup"
+                  id="signup"
+                  className="form-submit"
+                  value="Create Account"
+                />
+              </div>
+
+              <NavLink to="/login">
+                <span>Already have an account?</span>Sign in
+              </NavLink>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
