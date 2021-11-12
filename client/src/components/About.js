321e4913bcf357a455d50a9cdbb09dfe80ab1beb
Aditya Trivedi <adityakuldeeptrivedi@gmail.com>
2021-11-12 19:44:34 +0530

    about page structure done

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -30,7 +30,7 @@ const About = () => {
                   <li class="nav-item">
                     <a
                       class="nav-link"
-                      id="profileTab"
+                      id="profile-tab"
                       data-toggle="tab"
                       role="tab"
                       href="#profile"
@@ -48,6 +48,135 @@ const About = () => {
                 className="profileEdit"
                 value="Edit Profile"
               />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="workProfile">
+                <p>YOUR DESIGNATION</p>
+                <a
+                  href="https://github.com/AdityaTrivedi14"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Youtube
+                </a>{" "}
+                <br />
+                <a
+                  href="https://github.com/AdityaTrivedi14"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Instagram
+                </a>
+                <br />
+                <a
+                  href="https://github.com/AdityaTrivedi14"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Web Developer
+                </a>
+                <br />
+                <a
+                  href="https://github.com/AdityaTrivedi14"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Freelancer
+                </a>
+                <br />
+                <a
+                  href="https://github.com/AdityaTrivedi14"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Software Engineer
+                </a>
+                <br />
+              </div>
+            </div>
+            <div className="col-md-8 profileInfo">
+              <div className="tabContent profileTab" id="myTabContent">
+                <div
+                  className="tab-pane fade show active"
+                  id="home"
+                  role="tabpanel"
+                  aria-labelledby="profile-tab"
+                >
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>User Id</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>594380398675409780</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Name</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Aditya</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Email</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>adityakuldeeptrivedi@gmail.com</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Profession</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Web Developer</p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="tab-pane fade"
+                  id="profile"
+                  role="tabpanel"
+                  aria-labelledby="profile-tab"
+                >
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Hourly Rate</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>25$</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Name</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Aditya</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Email</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>adityakuldeeptrivedi@gmail.com</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <label>Profession</label>
+                    </div>
+                    <div className="col-md-6">
+                      <p>Web Developer</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </form>
