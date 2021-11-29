419ff050315c29daff6d69f9c5bf736cd3428f88
Aditya Trivedi <adityakuldeeptrivedi@gmail.com>
2021-11-29 16:35:33 +0530

    typo correction

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Logout from "./components/Logout";
 import PageNotFound from "./components/PageNotFound";
-import { intialState, reducer } from "../src/reducer/UseReducer";
+import { initialState, reducer } from "../src/reducer/UseReducer";
 
 export const UserContext = createContext();
 
